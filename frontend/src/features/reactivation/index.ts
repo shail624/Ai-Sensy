53272fe5aff2bb97408df27581f75fe8e05ab693
shail624 <del353100@gmail.com>
2026-08-04 07:59:29 +0530

    feat(reactivation): export mission-control workspaces

--- frontend/src/features/reactivation/index.ts
+++ frontend/src/features/reactivation/index.ts
@@ -1,4 +1,10 @@
-export { REACTIVATION_SECTIONS, type ReactivationSection } from "./sections";
+export {
+  REACTIVATION_SECTIONS,
+  REACTIVATION_LIVE_SECTIONS,
+  REACTIVATION_FOUNDATION_SECTIONS,
+  type ReactivationSection,
+} from "./sections";
 export { DocumentCenter } from "./DocumentCenter";
+export { ReactivationMissionControl } from "./ReactivationMissionControl";
 export { ReactivationPipelineBoard, REACTIVATION_STAGE_BLUEPRINT } from "./ReactivationPipelineBoard";
 export { ReactivationReports } from "./ReactivationReports";
